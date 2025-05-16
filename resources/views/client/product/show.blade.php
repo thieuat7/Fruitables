f60b96a52065413b2c59917545bdfcf4d85be830
thieuat7 <49011041042@student.hcmue.edu.vn>
2025-05-16 18:35:37 +0700

    update

--- resources/views/client/product/show.blade.php
+++ resources/views/client/product/show.blade.php
@@ -10,29 +10,29 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap"
-        rel="stylesheet">
-    <!--Sử dụng thư viện jQuery Toast:-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
-    <!-- Icon Font Stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
-    <link href="{{ secure_asset('lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('lib/owlcarousel/secure_assets/owl.carousel.min.css') }}" rel="stylesheet">
+<!-- jQuery Toast Plugin -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
 
+<!-- Icon Fonts -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
 
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<!-- Libraries Stylesheets -->
+<link href="{{ secure_asset('lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
-    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+<!-- Customized Bootstrap Stylesheet -->
+<link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+<!-- Main Template Stylesheet -->
+<link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+
 
     <style>
     .pagination {
@@ -364,13 +364,15 @@
 
 
     <!-- JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
-    <script src="{{ asset('lib/lightbox/js/lightbox.min.js') }}"></script>
-    <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
-    <!-- Template Javascript -->
-    <script src="{{ asset('js/main.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ secure_asset('lib/easing/easing.min.js') }}"></script>
+<script src="{{ secure_asset('lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ secure_asset('lib/lightbox/js/lightbox.min.js') }}"></script>
+<script src="{{ secure_asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+<!-- Template Javascript -->
+<script src="{{ secure_asset('js/main.js') }}"></script>
+
     
 </body>
 
